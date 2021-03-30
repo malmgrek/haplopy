c6e820f451d221c54da9065048c4431d2d6c4f3f
Stratos Staboulis <stratos.staboulis@leanheat.com>
2021-03-31 00:49:13 +0300

    Rename main module

--- haplopy/model.py
+++ haplopy/model.py
@@ -1,49 +1,40 @@
-# -*- coding: utf-8 -*-
+"""Haplotype imputation model
 
-"""
-haplopy.main
-~~~~~~~~~~~~
-
-This module implements the HaploPy
-
-:copyright: (c) 2019 by Stratos Staboulis
-:license: MIT
 """
 
 import collections
+from functools import reduce
 import itertools
 import logging
+from typing import Dict, List, Set
 
 import numpy as np
 from scipy.sparse import dok_matrix
 
 
-logger = logging.getLogger(__name__)
-
-
-__version__ = '0.0.1.dev'
-
-
 # TODO Randomized initial guess in iteration
 # TODO Imputation example
+# TODO Try BayesPy: We can directly use analysis content:
+#      - genotype_expansion gives the "draws" such that each
+#        index is a "color". Sample numbers are obtained by
+#        multiplying each index occurrences with "counts". Perhaps
+#        collections.Counter can be used more directly.
+#      - Maybe reduce the itertools.product thingy.
 # TODO Unit-tests
 
 
-def log_binomial(n, m):
-    """Logarithm of binomial coefficient using Stirling approximation
+class HaplotypeMultinomial():
 
-    """
-    return n * np.log(n) - m * np.log(m) - (n - m) * np.log(n - m) + \
-           0.5 * (np.log(n) - np.log(m) - np.log(n - m) - np.log(2 * np.pi))
+    def __init__(self, p_haplotypes: Dict[str, float]=None):
+        self.p_haplotypes = p_haplotypes
+        return
 
+    def fit(self, phenotypes: Dict[str, float]):
+        """Expectation Maximization
 
-def log_multinomial(*args):
-    """Logarithm of the multinomial coefficient
-
-    """
-    if len(args) == 1:
-        return 1
-    return log_binomial(sum(args), args[-1]) + log_multinomial(args[:-1])
+        """
+        p_haplotypes = None
+        return HaplotypeMultinomial(p_haplotypes)
 
 
 class PhenotypeData(object):
@@ -55,8 +46,11 @@ class PhenotypeData(object):
 
     # TODO code example
 
-    :param sample: Measured unphased genotype data
-    :type sample:
+    Parameters
+    ----------
+
+    sample : List[Tuple[str]]
+        Measured unphased genotype data
 
     """
     def __init__(self, sample=None):
@@ -76,12 +70,13 @@ class PhenotypeData(object):
 
         # TODO code example
 
-        :param proba_haplotypes: Known haplotype frequencies
-        :type proba_haplotypes: dict
-        :param nobs: Number of generated observations
-        :type nobs: int
-        :return: Instance of :class:`PhenotypeData` with the simulated data
-                 in :attr:`sample`
+        Parameters
+        ----------
+
+        proba_haplotypes : dict[str, float]
+            Known haplotype frequencies
+        nobs : int
+            Number of generated observations
 
         """
         haplotypes = list(proba_haplotypes)
@@ -99,16 +94,13 @@ class PhenotypeData(object):
                       for x in zip(haplotypes[i],
                                    haplotypes[j]))
             )
-        logger.info('Simulated phenotype data of {0} observations'.format(
+        logging.info('Simulated phenotype data of {0} observations'.format(
             nobs))
         return cls(sample=sample)
 
     @property
-    def haplotypes(self):
+    def haplotypes(self) -> list:
         """All haplotypes compatible with :attr:`sample`
-
-        :return: Sequence of haplotypes in unspecified order.
-        :rtype: list
 
         """
         if self._haplotypes is None:
@@ -119,16 +111,12 @@ class PhenotypeData(object):
                     set(itertools.product(*[set(loc) for loc in phenotype]))
                 )
             self._haplotypes = list(haplotypes)
-            logger.info('Constructed haplotypes data')
+            logging.info('Constructed haplotypes data')
         return self._haplotypes
 
     @property
-    def analysis(self):
-        """Analysis of the phenotype sample
-
-        :return: Useful information deduced from the phenotype data in
-                 :attr:`sample`.
-        :rtype: dict
+    def analysis(self) -> dict:
+        """Useful information deduced from the sample phenotype data
 
         """
         if self._analysis is None:
@@ -155,32 +143,28 @@ class PhenotypeData(object):
             self._analysis = {'counts': list(counter.values()),
                               'phenotypes': unique_phenotypes,
                               'genotype_expansion': genotype_expansion}
-            logger.info('Constructed phenotype analysis data')
+            logging.info('Constructed phenotype analysis data')
         return self._analysis
 
     @property
-    def genotypes(self):
+    def genotypes(self) -> list:
         """List all genotypes compatible with the phenotype sample
 
-        :return: List of genotypes compatible with the phenotype data in
-                 :attr:`sample`. The genotypes are referred to in terms of the
-                 natural index of :attr:`haplotypes`
-        :rtype: list
+        Genotypes are referred to in terms of the natural index of haplotypes
 
         """
         if self._genotypes is None:
             genotypes = [x for y in self.analysis['genotype_expansion']
                          for x in y]
             self._genotypes = genotypes
-            logger.info('Constructed genotypes data')
+            logging.info('Constructed genotypes data')
         return self._genotypes
 
     @property
     def genotype_matrix(self):
         """Count haplotype occurrences in each compatible genotype
 
-        :return: Sparse matrix of shape (num haplotypes) x (num genotypes)
-        :rtype: scipy.sparse.dok_matrix
+        Returns an instance of scipy.sparse.dok_matrix
 
         """
         if self._genotype_matrix is None:
@@ -192,7 +176,7 @@ class PhenotypeData(object):
                 genotype_matrix[genotype[0], i] += 1
                 genotype_matrix[genotype[1], i] += 1
             self._genotype_matrix = genotype_matrix
-            logger.info('Constructed genotype occurrence matrix')
+            logging.info('Constructed genotype occurrence matrix')
         return self._genotype_matrix
 
     @property
@@ -203,16 +187,22 @@ class PhenotypeData(object):
         return self._log_likelihood_const
 
 
-def expectation_maximization_update(phenotype_data, proba_haplotypes):
+def expectation_maximization_update(
+        phenotype_data,
+        proba_haplotypes
+) -> np.ndarray:
     """Progress the EM algorithm one step forward
 
-    :param phenotype_data: Pre-calculated data structure of the phenotype data
-    :type phenotype_data: :class:`~haplopy.PhenotypeData`
-    :param proba_haplotypes: Current estimate of the haplotype frequencies
-    :type proba_haplotypes: :class:`~numpy.array`
-    :return: Next estimate of the haplotype probabilities and _previous_
-             Log-Likelihood
-    :rtype: (:class:`numpy.ndarray`, float)
+    Calculates the next estimate of haplotype probabilities and _previous_
+    Log-likelihood
+
+    Parameters
+    ----------
+
+    phenotype_data : PhenotypeData
+        Pre-calculated data structure of the phenotype data
+    proba_haplotypes : Dict[str, float]
+        Current estimate of the individual haplotype frequencies
 
     """
     nobs = sum(phenotype_data.analysis['counts'])  # number of observations
@@ -234,35 +224,39 @@ def expectation_maximization_update(phenotype_data, proba_haplotypes):
     return proba_haplotypes, log_likelihood
 
 
-def expectation_maximization(phenotype_data, proba_haplotypes=None,
-                             max_iter=20, tol=1.0e-5,
-                             logging_threshold=1.0e-5):
+def expectation_maximization(
+        phenotype_data,
+        proba_haplotypes=None,
+        max_iter=20,
+        tol=1.0e-5,
+        logging_threshold=1.0e-5
+) -> Dict[str, float]:
     """EM iteration for haplotype frequency estimation
 
     Calculates the Maximum Likelihood estimate of the haplotype frequencies.
+    The source data is unphased genotype data (phenotype).
 
     TODO Math formula of likelihood
     TODO Code example
 
-    The source data is unphased genotype data (phenotype).
+    Parameters
+    ----------
 
-    :param phenotype_data: Pre-calculated data structure of the phenotype data
-    :type phenotype_data: :class:`~haplopy.PhenotypeData`
-    :param proba_haplotypes: Initial estimate of the haplotype frequencies
-    :type proba_haplotypes: :class:`~numpy.array`
-    :param max_iter: Maximum number of iterations
-    :type max_iter: int
-    :param tol: Log Likelihood convergence tolerance
-    :type tol: float
-    :param logging_threshold: Threshold for logging small results
-    :type logging_threshold: float
-    :return: Estimated haplotype frequencies
-    :rtype: :class:`~numpy.array`
+    phenotype_data : PhenotypeData
+        Pre-calculated data structure of the phenotype data
+    proba_haplotypes : np.ndarray
+        Initial estimate of the individual haplotype frequencies
+    max_iter : int
+        Maximum number of iterations
+    tol : float
+        Log-likelihood convergence tolerance
+    logging_threshold : float
+        Threshold for logging small results
 
     """
-    logger.info('Start EM algorithm')
+    logging.info('Start EM algorithm')
     if proba_haplotypes is None:  # equal frequencies as initial guess
-        logger.info('Auto-generated uniform initial guess')
+        logging.info('Auto-generated uniform initial guess')
         nhaplotypes = len(phenotype_data.haplotypes)
         proba_haplotypes = np.ones(nhaplotypes) / nhaplotypes
     objective_values = list()
@@ -273,25 +267,25 @@ def expectation_maximization(phenotype_data, proba_haplotypes=None,
             phenotype_data, proba_haplotypes
         )
         objective_values.append(log_likelihood)
-        logger.info(
+        logging.info(
             'Iteration {0} | log(L) = {1:.4e} | delta = {2:.4e}'.format(
                 i, log_likelihood, delta)
         )
         if i > 0:
             delta = objective_values[-1] - objective_values[-2]
         if delta < tol:
-            logger.info('Convergence up to tolerance level {0}'.format(tol))
+            logging.info('Convergence up to tolerance level {0}'.format(tol))
             break
         i += 1
     res = {''.join(k): v for k, v in zip(phenotype_data.haplotypes,
                                          proba_haplotypes)}
     if i == max_iter:
-        logger.info('EM algorithm terminated after maximum number of {0} '
+        logging.info('EM algorithm terminated after maximum number of {0} '
                     'iterations.'.format(i))
     # Additional log message for results
     msg = '\n\nResults\n' +\
               '=======\n' +\
           ''.join(['{0} | {1:.6f}\n'.format(k, v)
                    for k, v in res.items() if v > logging_threshold])
-    logger.debug(msg)
+    logging.debug(msg)
     return res
